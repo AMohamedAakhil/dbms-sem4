7abe7588a169383f1164331d7910ab68cec69291
A Mohamed Aakhil <a.aakhilmohamed@gmail.com>
2024-03-21 14:56:11 +0530

    feat: add appropriate redirects to login page

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,6 +1,12 @@
+import { getServerAuthSession } from '@/server/auth';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const Admin = () => {
+const Admin = async () => {
+  const session = await getServerAuthSession();
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <div>Admin</div>
   )
--- src/app/(admin)/admin/products/page.tsx
+++ src/app/(admin)/admin/products/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ProductTable from './product-view-table/task-view-table'
+import { getServerAuthSession } from '@/server/auth';
+import { redirect } from 'next/navigation';
 
-const Products = () => {
+const Products = async () => {
+  const session = await getServerAuthSession();
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <div>
         <ProductTable />
